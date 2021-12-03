d77810b6001c837bff125f5e7210514618270389
Павила Падаван <vaambival@gmail.com>
2021-12-03 16:22:53 +0300

    Gateway

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # gateway
 Gateway Application
+## Поставка на k8s
+
+1. Создать докер образ локально командой `docker build -t <container-name> . ` - `container_name` имя контейнера.
+Например:
+```
+docker build -t digital-twin-gateway . 
+```
+
+2. Протегать контейнер командой `docker tag <container-name> <docker-hub>/<container-name-for-docker-hub>:<version>`- 
+где `<container-name>`  - имя контейнера из 1-го шага, `<docker-hub>` - логин для docker hub, `<container-name-for-docker-hub>` - 
+имя контейнера для публикации в докер-хабе, `<version>` - версия. Например:
+```
+docker tag digital-twin-gateway vladimirkurovx5/digital-twin-gateway:1.0.9
+```
+3. Отправить контейнер в докер хаб командой `docker push <docker-hub>/<container-name-for-docker-hub>:<version>`
+Например:
+```
+docker push vladimirkurovx5/digital-twin-gateway:1.0.8
+```
+4. В файле [kube](kube/gateway.yaml) обновить версию образа докера
+5. Выполнить команду 
+```
+kubectl apply -f <path-to-kube-directory> --kubeconfig=<path-to-kube-config>
+```
+где необходимо указать путь до директории [kube](kube) и путь до файла с конфигами для подключения к стенду
--- src/main/java/ru/eatit/gateway/controller/HealthCheckController.java
+++ src/main/java/ru/eatit/gateway/controller/HealthCheckController.java
@@ -3,8 +3,12 @@ package ru.eatit.gateway.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,18 +21,21 @@ public class HealthCheckController {
     }
 
     @GetMapping("hd")
-    public void hd() {
+    public void hd(@RequestParam("path") String path) {
         try {
             Configuration conf = new Configuration();
             conf.set("hadoop.job.ugi", "team14");
+            // user = ?
             conf.addResource(new Path("conf/core-site.xml"));
             conf.addResource(new Path("conf/hdfs-site.xml"));
+            conf.set("fs.hdfs.impl", DistributedFileSystem.class.getName());
+            System.setProperty("HADOOP_USER_NAME", "team14");
             FileSystem fileSystem = FileSystem.get(conf);
             log.info("Подключились к: " + fileSystem.getUri());
-            Path path = new Path("/usr/src/app/Pavel");
-            log.info("LS: " + fileSystem.listFiles(new Path(""), false));
-            boolean rez = fileSystem.mkdirs(path);
-            log.info("Rez + " + rez);
+            RemoteIterator<LocatedFileStatus> iterator = fileSystem.listFiles(new Path(path), false);
+            while (iterator.hasNext()) {
+                log.info(iterator.next().getPath().toString());
+            }
             fileSystem.close();
         } catch (Exception exc) {
             log.error("Exception Pavil: ", exc);
