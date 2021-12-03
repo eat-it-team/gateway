6d72c9ee79a5af8369cb3073b7b8aeb666cbaa3c
Павила Падаван <vaambival@gmail.com>
2021-12-03 19:00:47 +0300

    Gateway

--- src/main/java/ru/eatit/gateway/service/TvsService.java
+++ src/main/java/ru/eatit/gateway/service/TvsService.java
@@ -1,6 +1,8 @@
 package ru.eatit.gateway.service;
 
+import feign.FeignException.FeignClientException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.eatit.gateway.controller.entity.response.TvsUserDto;
@@ -13,6 +15,10 @@ public class TvsService {
     private final TvsClient tvsClient;
 
     public ResponseEntity<TvsUserDto> findUser(String accessToken) {
-        return tvsClient.find(accessToken);
+        try {
+            return tvsClient.find(accessToken);
+        } catch (FeignClientException exc) {
+            return new ResponseEntity<>(HttpStatus.resolve(exc.status()));
+        }
     }
 }
